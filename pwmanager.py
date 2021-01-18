aff32890727789f5483301f28bbf22b093b59743
daniel wang <daniel.wang13@hotmail.com>
2021-01-18 21:27:47 +1100

    connected to database

--- pwmanager.py
+++ pwmanager.py
@@ -1,1 +1,10 @@
 import mysql.connector
+
+db_connection = mysql.connector.connect(
+    host="localhost", 
+    user="root",
+    password="SQLpassWord789",
+    auth_plugin='mysql_native_password',
+    )
+
+print(db_connection)
