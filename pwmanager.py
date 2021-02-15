a7b2906c7246b99d688fde0c823dc484c4ddaa95
daniel wang <daniel.wang13@hotmail.com>
2021-02-15 12:12:09 +1100

    inserting data into database

--- pwmanager.py
+++ pwmanager.py
@@ -5,6 +5,29 @@ db_connection = mysql.connector.connect(
     user="root",
     password="SQLpassWord789",
     auth_plugin='mysql_native_password',
+    database="pwords",
     )
 
-print(db_connection)
+# checking if connection is successful
+# print(db_connection)
+
+my_cursor = db_connection.cursor()
+# my_cursor.execute("CREATE DATABASE testdb")
+
+# check if database has been created
+# my_cursor.execute("SHOW DATABASES")
+# for db in my_cursor:
+#     print(db[0])
+
+# my_cursor.execute("CREATE DATABASE pwords")
+
+# my_cursor.execute("CREATE TABLE passwords (website VARCHAR(255), username VARCHAR(255), pword VARCHAR(255))")
+# my_cursor.execute("SHOW TABLES")
+# for table in my_cursor:
+#     print(table[0])
+
+insertion = "INSERT INTO passwords (website, username, pword) VALUES (%s, %s, %s)"
+record1 = "facebook", "daniel1", "wang2"
+
+my_cursor.execute(insertion, record1)
+db_connection.commit()
